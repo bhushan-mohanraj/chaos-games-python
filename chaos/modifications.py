6185d45fac60eadfa6dc2ea07a6f8bad2b7007c6
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-02-23 00:55:11 -0500

    Add base classes for the modification mixins.

--- chaos/modifications.py
+++ chaos/modifications.py
@@ -2,4 +2,27 @@
 Modification mixins extending the traditional chaos game.
 """
 
+import abc
+import functools
+
 import chaos.game
+
+
+class VertexesModification(abc.ABC):
+    """
+    The base class for mixins which modify the vertexes.
+    """
+
+    @abc.abstractmethod
+    def _get_vertexes(self):
+        pass
+
+
+class NextVertexModification(abc.ABC):
+    """
+    The base class for mixins which modify the next vertex.
+    """
+
+    @abc.abstractmethod
+    def _get_next_vertex_index(self, vertex_indexes: list[int]):
+        pass
