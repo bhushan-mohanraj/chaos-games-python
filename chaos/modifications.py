6a72588edaeb6d86ef073a5c74171475dd7571d1
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-02-25 09:38:40 -0500

    Add missing functions to base modification classes.

--- chaos/modifications.py
+++ chaos/modifications.py
@@ -17,11 +17,19 @@ class VertexesModification(abc.ABC):
     def get_vertexes(self):
         pass
 
+    @abc.abstractmethod
+    def get_next_vertex_index(self, selected_vertex_indexes: list[int]):
+        pass
+
 
 class NextVertexModification(abc.ABC):
     """
     The base class for mixins which modify the next vertex.
     """
+
+    @abc.abstractmethod
+    def get_vertexes(self):
+        pass
 
     @abc.abstractmethod
     def get_next_vertex_index(self, selected_vertex_indexes: list[int]):
