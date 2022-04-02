09deb1488e04ceed0d1f0898995b16b113c63ac1
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-04-02 08:39:54 -0400

    Organize the `examples` package.

--- examples/__main__.py
+++ examples/__main__.py
@@ -4,23 +4,15 @@ Create and plot the examples of chaos games.
 
 import pathlib
 
-import examples.sierpinski_triangle_game
-import examples.sierpinski_pentagon_game
+import examples._triangle
+import examples._pentagon
 
 
-# All the examples of chaos games.
-# Each tuple contains the game instance and the filename for saving.
-games = [
-    (
-        examples.sierpinski_triangle_game.sierpinski_triangle_game,
-        "sierpinski_triangle_game.png",
-    ),
-    (
-        examples.sierpinski_pentagon_game.sierpinski_pentagon_game,
-        "sierpinski_pentagon_game.png",
-    ),
-]
+game_filenames = {
+    "triangle_sierpinski.png": examples._triangle.sierpinski_triangle_game,
+    "pentagon_sierpinski.png": examples._pentagon.sierpinski_pentagon_game,
+}
 
 # Run each game and produce the corresponding plot.
-for game, filename in games:
+for filename, game in game_filenames.items():
     game.plot(pathlib.Path(__file__).parent / filename)
